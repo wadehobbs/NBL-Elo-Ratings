61c23a5558442a7174f595fe439caa8fa19629f3
Wade <wade.hobbs87@gmail.com>
2018-09-19 21:28:09 +1000

    Updates to R code

--- NBL_elo_code.R
+++ NBL_elo_code.R
@@ -1,7 +1,6 @@
 ####----Web Scraping NBL data----#### FAILED ATTEMPT
 library(XML)
 library(stringr)
-library(tidyverse)
 
 #Set URL
 url <- read_html("https://www.nbl.com.au/results")
@@ -30,12 +29,13 @@ test <- url2 %>%
 #https://www.flashscore.com/basketball/australia/nbl/results/
 
 ####----Cleaning and manipulating data----####
+library(tidyverse)
 
 #Import data
 nbl_db <- read_csv("raw_nbl_data.csv")
 #Clean: separate the score and date columns 
-nbl_db <- separate(data_2016, Score, into = c("Home_Score", "Away_Score"), sep = "\\:", remove = TRUE)
-nbl_db <- separate(data_2016, Date, into = c("Date", "Time"), sep = "\\. ", remove = TRUE)
+nbl_db <- separate(nbl_db, Score, into = c("Home_Score", "Away_Score"), sep = "\\:", remove = TRUE)
+nbl_db <- separate(nbl_db, Date, into = c("Date", "Time"), sep = "\\. ", remove = TRUE)
 #Delete incomplete rows
 nbl_db <- nbl_db[complete.cases(nbl_db),]
 #Trim empty space on the away_score col
@@ -68,7 +68,7 @@ date[121:171] <- sub("$", ".18", date[121:171] )
 date[172:241] <- sub("$", ".17", date[172:241] )
 nbl_db$Date <- date
 nbl_db$Date <- dmy(nbl_db$Date) #Function converts to date
-arrange(nbl_db, Date)
+nbl_db <- arrange(nbl_db, Date)
 
 ####----Elo ratings calculations----####
 library(elo)
@@ -80,8 +80,7 @@ nbl_db <- nbl_db %>%
                                 if_else(Home_Score == Away_Score, 0.5, 0)))
 #Run elo.run function over data
 #This returns an object of class 'elo.run' - a list which is largely unusable without modification
-
-nbl_elo_calcs <- elo.run(nbl_db$result ~ nbl_db$Home + nbl_db$Away, data = nbl_db, k = 20)
+nbl_elo_calcs_std <- elo.run(nbl_db$result ~ nbl_db$Home + nbl_db$Away, data = nbl_db, k = 20)
 
 #Take the elo.run object, sum results using the final.elos function and set as a data frame
 nbl_elo_ratings <- data.frame(Elo.Ratings = final.elos(nbl_elo_calcs))
@@ -94,13 +93,22 @@ nbl_elo_ratings <- arrange(nbl_elo_ratings, desc(Elo.Ratings))
 
 #This could be more accurate by taking into account the home court advantage. 
 #100 is a standard set by fivethirtyeight from NBA data, good as an estimate 
-nbl_elo_calcs <- elo.run(nbl_db$result ~ adjust(nbl_db$Home, 100) + nbl_db$Away, data = nbl_db, k = 20)
+
+nbl_elo_calcs_adj4hc <- elo.run(nbl_db$result ~ adjust(nbl_db$Home, 100) + nbl_db$Away, data = nbl_db, k = 20)
+nbl_elo_ratings_adj4hc <- data.frame(Elo.Ratings = final.elos(nbl_elo_calcs_adj4hc))
+nbl_elo_ratings_adj4hc <- rownames_to_column(nbl_elo_ratings_adj4hc, "Teams")
+nbl_elo_ratings_adj4hc <- arrange(nbl_elo_ratings_adj4hc, desc(Elo.Ratings))
+
+#Results in slight changes but nothing major with the 2 seasons of data provided
 
 ####--Account for winning margin--####
 
+#Basic way to account for margin - can introduce autocorrelation
+nbl_elo_calcs <- elo.run(nbl_db$result ~ adjust(nbl_db$Home, 100)+ nbl_db$Away + k(20*log(G) + 1), data = nbl_db)
 #Set up the initial team elo values data frame
 teams <- data.frame(team = unique(c(nbl_db$Home, nbl_db$Away)))
 
+#FiveThirtyEight Method using elo.calc and a for loop
 #Set intial values to 1500 (Standard in elo ratings)
 teams <- teams %>%
         mutate(elo = 1500)
@@ -122,10 +130,10 @@ for (i in seq_len(nrow(nbl_db))) {
         new_elo <- elo.calc(wins.A = game$result,
                             elo.A = teamA_elo,
                             elo.B = teamB_elo,
-                            k = 20, adjust.B = (((game$G + 3)^0.8) / (7.5 + 0.006*(
+                            k = 20*(((game$G + 3)^0.8) / (7.5 + 0.006*(
                                     ifelse(game$Home_Score > game$Away_Score, teamA_elo, teamB_elo) - 
-                                            ifelse(game$Home_Score > game$Away_Score, teamB_elo, teamA_elo) + 
-                                                ifelse(game$Home_Score > game$Away_Score, 100, -100)))))
+                                             ifelse(game$Home_Score > game$Away_Score, teamB_elo, teamA_elo) + 
+                                                 ifelse(game$Home_Score > game$Away_Score, 100, -100)))))
         
         # The results come back as a data.frame
         # with team A's new rating in row 1 / column 1
@@ -138,4 +146,7 @@ for (i in seq_len(nrow(nbl_db))) {
         teams <- teams %>%
                 mutate(elo = if_else(team == game$Home, teamA_new_elo,
                                      if_else(team == game$Away, teamB_new_elo, elo)))
+        teams <- arrange(teams, desc(elo))
 }
+
+
